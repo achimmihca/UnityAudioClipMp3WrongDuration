23259cde788298dd17934ac4e7fda514790be500
Andreas Stange <anst.dev@gmail.com>
2023-07-13 14:48:57 +0200

    initial commit

---
+++ Assets/Scenes/SampleSceneControl.cs
@@ -0,0 +1,57 @@
+using System;
+using System.IO;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SampleSceneControl : MonoBehaviour
+{
+    private void Start()
+    {
+        string relativePath = "Scenes/mp3-TestAudio-4sec.mp3";
+        string fullPath = Path.Combine(Application.dataPath, relativePath);
+
+        AudioClip audioClip = LoadAudioClipImmediately(fullPath, true);
+
+        float expectedLengthInSeconds = 4;
+        Debug.Log($"audioClip.length: {audioClip.length} seconds, expected length: {expectedLengthInSeconds} seconds");
+        if (Math.Abs(audioClip.length - expectedLengthInSeconds) > 0.1f)
+        {
+            Debug.LogError("audioClip.length is not as expected.");
+        }
+    }
+
+    public static AudioClip LoadAudioClipImmediately(string uri, bool streamAudio)
+    {
+        Uri uriHandle = new Uri(uri);
+        using UnityWebRequest webRequest = CreateAudioClipRequest(uriHandle, streamAudio);
+        webRequest.SendWebRequest();
+
+        while (!webRequest.isDone)
+        {
+            Debug.LogWarning("Waiting for AudioClip to load via Thread.Sleep");
+            Thread.Sleep(10);
+        }
+
+        if (webRequest.result
+            is UnityWebRequest.Result.ConnectionError
+            or UnityWebRequest.Result.ProtocolError)
+        {
+            Debug.LogError("Error Loading Audio: " + uri);
+            Debug.LogError(webRequest.error);
+        }
+
+        AudioClip audioClip = (webRequest.downloadHandler as DownloadHandlerAudioClip)?.audioClip;
+        string fileName = Path.GetFileName(uriHandle.LocalPath);
+        audioClip.name = $"Audio file '{fileName}'";
+        return audioClip;
+    }
+
+    public static UnityWebRequest CreateAudioClipRequest(Uri uriHandle, bool streamAudio)
+    {
+        UnityWebRequest webRequest = UnityWebRequestMultimedia.GetAudioClip(uriHandle, AudioType.UNKNOWN);
+        DownloadHandlerAudioClip downloadHandler = webRequest.downloadHandler as DownloadHandlerAudioClip;
+        downloadHandler.streamAudio = streamAudio;
+        return webRequest;
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+#  UnityAudioClipMp3WrongDuration
+Test loading AudioClip in Unity at runtime.
+Sadly, some mp3 files have a wrong duration.
+
+See also the [Unity forum](https://forum.unity.com/threads/audioclip-length-is-incorrect-when-loading-from-webrequest-getaudioclip.1082183/)
